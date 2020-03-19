0abc304264626bf35ff85bff5b29b748dfaec9de
Carlos Gasperi <cgasperi90@gmail.com>
2020-03-19 15:12:14 -0400

    adding code to javascript file that appends all the data that the user inputs onto the html page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,30 @@ var config = {
 };
 
 firebase.initializeApp(config);
+
 var database = firebase.database();
 
-//now we will make our variables that will go into the database
-    
+//here we will make a on click function for when the user clicks the submit button
+$("#submit-button").on("click", function(event) {
+
+    event.preventDefault();
+
+    var time = $("#time-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrain = $("#first-train-time").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+
+    database.ref().push({
+        time: time,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    });
+
+});
+
+database.ref().on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+
+    $("table").append("<tr><td>" + snapshot.val().time + "</td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().firstTrain + "</td><td>" + snapshot.val().frequency + "</td></tr>")
+});
